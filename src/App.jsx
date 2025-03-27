a700380257754b06be6a2e2e40d40acdf56b1ece
MatiVolpe <mati.clc.volpe@gmail.com>
2025-03-26 21:58:51 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Typography variant='h1'>Hola hola</Typography>
+    </>
+  )
+}
+
+export default App
